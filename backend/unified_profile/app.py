4c2bdd962566411cdb81640756798bb2e0ab7a42
shiinalight <nooshin.pourkamali@gmail.com>
2026-04-10 08:03:31 +0200

    update backend CORS settings

--- backend/unified_profile/app.py
+++ backend/unified_profile/app.py
@@ -14,6 +14,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8081",
         "http://127.0.0.1:8081",
+        "http://localhost:8082",
+        "http://127.0.0.1:8082",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
